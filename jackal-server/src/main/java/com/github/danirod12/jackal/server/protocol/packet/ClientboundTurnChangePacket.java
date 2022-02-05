39c52d4cbb9bfcf3d43ee271ff7aab1e1adcdba6
Den Bekker <69750264+danirod12@users.noreply.github.com>
2022-02-05 18:27:57 +0300

    Added ClientboundTurnChangePacket

---
+++ jackal-server/src/main/java/com/github/danirod12/jackal/server/protocol/packet/ClientboundTurnChangePacket.java
@@ -0,0 +1,23 @@
+package com.github.danirod12.jackal.server.protocol.packet;
+
+import com.github.danirod12.jackal.server.protocol.ServerSideConnection;
+
+public class ClientboundTurnChangePacket extends NamedPacket {
+
+    private final String data;
+
+    /**
+     * Send turn change for players.
+     * @param cooldown_ends is optional
+     */
+    public ClientboundTurnChangePacket(ServerSideConnection connection, long cooldown, long cooldown_ends) {
+        super(40);
+        this.data = connection.getName() + ":" + cooldown + ":" + (cooldown_ends < 0 ? System.currentTimeMillis() : cooldown_ends);
+    }
+
+    @Override
+    public String data() {
+        return data;
+    }
+
+}

553b4e55809f7180378f5070f595dc1a5071d286
Den Bekker <69750264+danirod12@users.noreply.github.com>
2022-02-05 18:31:29 +0300

    Fixed ClientboundTurnChangePacket cooldown

--- jackal-server/src/main/java/com/github/danirod12/jackal/server/protocol/packet/ClientboundTurnChangePacket.java
+++ jackal-server/src/main/java/com/github/danirod12/jackal/server/protocol/packet/ClientboundTurnChangePacket.java
@@ -8,11 +8,12 @@ public class ClientboundTurnChangePacket extends NamedPacket {
 
     /**
      * Send turn change for players.
+     * @param cooldown in ticks (1 second = 20 ticks)
      * @param cooldown_ends is optional
      */
     public ClientboundTurnChangePacket(ServerSideConnection connection, long cooldown, long cooldown_ends) {
         super(40);
-        this.data = connection.getName() + ":" + cooldown + ":" + (cooldown_ends < 0 ? System.currentTimeMillis() : cooldown_ends);
+        this.data = connection.getName() + ":" + cooldown + ":" + (cooldown_ends < 0 ? System.currentTimeMillis() + cooldown * 50 : cooldown_ends);
     }
 
     @Override
